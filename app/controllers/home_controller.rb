fc171aa187026396009c8a9f19861a3a258cc119
Kevin McPhillips <github@kevinmcphillips.ca>
2013-04-11 15:35:26 -0500

    Add a no-title page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,18 @@
 class HomeController < ApplicationController
-  
+
   def index
   end
-  
+
   def modals
   end
-  
+
   def default
   end
-  
+
   def no_icon
   end
-  
+
+  def no_title
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Iframedemo::Application.routes.draw do
 
   root to: "home#index"
-  
-  [:modals, :default, :no_icon].each do |action|
+
+  [:modals, :default, :no_icon, :no_title].each do |action|
     get action.to_s => "home##{action}"
   end
-  
+
   resources :unicorns
-  
+
 end
